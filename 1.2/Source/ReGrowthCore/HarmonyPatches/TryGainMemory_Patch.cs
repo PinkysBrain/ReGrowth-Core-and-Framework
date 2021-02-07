2d0ab57baf6144bb9221de17f9e5e4932ed7a2c6
Taranchuk <nfhfyxer@gmail.com>
2021-02-07 14:54:10 +0200

    update

--- 1.2/Source/ReGrowthCore/HarmonyPatches/TryGainMemory_Patch.cs
+++ 1.2/Source/ReGrowthCore/HarmonyPatches/TryGainMemory_Patch.cs
@@ -8,26 +8,31 @@ using Verse;
 namespace ReGrowthCore
 {
 
-	[HarmonyPatch(typeof(MemoryThoughtHandler), "TryGainMemory", new Type[]
-	{
-		typeof(Thought_Memory),
-		typeof(Pawn)
-	})]
-	public static class TryGainMemory_Patch
-	{
-		private static void Postfix(MemoryThoughtHandler __instance, ref Thought_Memory newThought, Pawn otherPawn)
-		{
-			var options = newThought.def.GetModExtension<ThoughtExtensions>();
-			if (options != null)
+    [HarmonyPatch(typeof(MemoryThoughtHandler), "TryGainMemory", new Type[]
+    {
+                typeof(Thought_Memory),
+                typeof(Pawn)
+    })]
+    public static class TryGainMemory_Patch
+    {
+        private static void Postfix(MemoryThoughtHandler __instance, ref Thought_Memory newThought, Pawn otherPawn)
+        {
+            Log.Message("TryGainMemory_Patch - Postfix - var options = newThought.def.GetModExtension<ThoughtExtensions>(); - 1", true);
+            var options = newThought.def.GetModExtension<ThoughtExtensions>();
+            Log.Message("TryGainMemory_Patch - Postfix - if (options != null) - 2", true);
+            if (options != null)
             {
-				if (options.removeThoughtsWhenAdded != null)
+                Log.Message("TryGainMemory_Patch - Postfix - if (options.removeThoughtsWhenAdded != null) - 3", true);
+                if (options.removeThoughtsWhenAdded != null)
                 {
-					foreach (var thoughtDef in options.removeThoughtsWhenAdded)
+                    Log.Message("TryGainMemory_Patch - Postfix - foreach (var thoughtDef in options.removeThoughtsWhenAdded) - 4", true);
+                    foreach (var thoughtDef in options.removeThoughtsWhenAdded)
                     {
-						__instance.pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(thoughtDef);
+                        Log.Message("TryGainMemory_Patch - Postfix - __instance.pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(thoughtDef); - 5", true);
+                        __instance.pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(thoughtDef);
                     }
                 }
             }
-		}
-	}
+        }
+    }
 }
