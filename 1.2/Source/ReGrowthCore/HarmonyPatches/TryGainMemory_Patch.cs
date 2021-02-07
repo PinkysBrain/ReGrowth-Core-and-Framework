3bf232914586f789d7f828d4b0a370dbc2c35b84
Taranchuk <nfhfyxer@gmail.com>
2021-02-07 14:55:01 +0200

    update

--- 1.2/Source/ReGrowthCore/HarmonyPatches/TryGainMemory_Patch.cs
+++ 1.2/Source/ReGrowthCore/HarmonyPatches/TryGainMemory_Patch.cs
@@ -17,18 +17,13 @@ namespace ReGrowthCore
     {
         private static void Postfix(MemoryThoughtHandler __instance, ref Thought_Memory newThought, Pawn otherPawn)
         {
-            Log.Message("TryGainMemory_Patch - Postfix - var options = newThought.def.GetModExtension<ThoughtExtensions>(); - 1", true);
             var options = newThought.def.GetModExtension<ThoughtExtensions>();
-            Log.Message("TryGainMemory_Patch - Postfix - if (options != null) - 2", true);
             if (options != null)
             {
-                Log.Message("TryGainMemory_Patch - Postfix - if (options.removeThoughtsWhenAdded != null) - 3", true);
                 if (options.removeThoughtsWhenAdded != null)
                 {
-                    Log.Message("TryGainMemory_Patch - Postfix - foreach (var thoughtDef in options.removeThoughtsWhenAdded) - 4", true);
                     foreach (var thoughtDef in options.removeThoughtsWhenAdded)
                     {
-                        Log.Message("TryGainMemory_Patch - Postfix - __instance.pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(thoughtDef); - 5", true);
                         __instance.pawn.needs.mood.thoughts.memories.RemoveMemoriesOfDef(thoughtDef);
                     }
                 }
