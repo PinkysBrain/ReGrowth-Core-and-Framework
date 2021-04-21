0f94be4c03817e82a8d6bbbe7dbe81223f029211
Taranchuk <nfhfyxer@gmail.com>
2021-04-21 19:57:04 +0300

    update

--- 1.2/Source/ReGrowthCore/Buildings/Building_AncientGraveFull.cs
+++ 1.2/Source/ReGrowthCore/Buildings/Building_AncientGraveFull.cs
@@ -30,6 +30,25 @@ namespace ReGrowthCore
                         this.innerContainer.TryAddOrTransfer(buriedPawn.Corpse);
                     }
                 }
+
+                // here is a bugfix for my silly bug with saving pawn, instead of corpse
+                var pawn = this.innerContainer.FirstOrDefault(x => x is Pawn);
+                if (pawn is Pawn corpse)
+                {
+                    this.innerContainer.Remove(pawn);
+
+                    var options = this.def.GetModExtension<AncientGraveExtensions>();
+                    if (options?.pawnKindDefsToSpawnInside != null && options.pawnKindDefsToSpawnInside.Any(x => x != null))
+                    {
+                        var pawnKind = options.pawnKindDefsToSpawnInside.Where(x => x != null).RandomElement();
+                        var buriedPawn = PawnGenerator.GeneratePawn(pawnKind);
+                        buriedPawn.Kill(null);
+                        buriedPawn.Corpse.Age = Rand.RangeInclusive(50, 300) * GenDate.DaysPerYear * GenDate.TicksPerDay;
+                        var compRottable = buriedPawn.Corpse.GetComp<CompRottable>();
+                        compRottable.RotProgress = compRottable.PropsRot.TicksToDessicated + 100000;
+                        this.innerContainer.TryAddOrTransfer(buriedPawn.Corpse);
+                    }
+                }
             }
             catch { };
         }
