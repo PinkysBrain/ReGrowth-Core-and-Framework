c4a7b90bd8d667ec11d3d38148ea5b1110ff018f
KylianB <kylian.balde@gmail.com>
2022-02-25 11:16:03 +0100

    Remove leaves spawner comps

--- 1.3/Source/ReGrowthCore/MultiplayerSupport.cs
+++ 1.3/Source/ReGrowthCore/MultiplayerSupport.cs
@@ -1,44 +1,39 @@
-﻿using System;
-using System.Reflection;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Multiplayer.API;
 using Verse;
 
 namespace ReGrowthCore
 {
-	[StaticConstructorOnStartup]
-	internal static class MultiplayerSupport
-	{
-		static MultiplayerSupport()
-		{
-			if (!MP.enabled)
-			{
-				return;
-			}
-			MP.RegisterSyncMethod(typeof(CompAutumnLeavesSpawner), "TryFindSpawnCell", null);
-			var tryFindSpawnCell = AccessTools.Method(typeof(CompAutumnLeavesSpawner), "TryFindSpawnCell", null, null);
-			harmony.Patch(tryFindSpawnCell, new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPre", null), new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPos", null), null, null);
+    [StaticConstructorOnStartup]
+    internal static class MultiplayerSupport
+    {
+        static MultiplayerSupport()
+        {
+            if (!MP.enabled)
+            {
+                return;
+            }
 
-			MP.RegisterSyncMethod(typeof(WeatherOverlay_Hail), "DoDamage", null);
-			var doDamage = AccessTools.Method(typeof(WeatherOverlay_Hail), "DoDamage", null, null);
-			harmony.Patch(doDamage, new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPre", null), new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPos", null), null, null);
+            MP.RegisterSyncMethod(typeof(WeatherOverlay_Hail), "DoDamage", null);
+            var doDamage = AccessTools.Method(typeof(WeatherOverlay_Hail), "DoDamage", null, null);
+            harmony.Patch(doDamage, new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPre", null), new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPos", null), null, null);
 
-			MP.RegisterSyncMethod(typeof(DevilDust_Tornado), "Tick", null);
-			var devilDust_Tornado = AccessTools.Method(typeof(DevilDust_Tornado), "Tick", null, null);
-			harmony.Patch(devilDust_Tornado, new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPre", null), new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPos", null), null, null);
-		}
+            MP.RegisterSyncMethod(typeof(DevilDust_Tornado), "Tick", null);
+            var devilDust_Tornado = AccessTools.Method(typeof(DevilDust_Tornado), "Tick", null, null);
+            harmony.Patch(devilDust_Tornado, new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPre", null), new HarmonyMethod(typeof(MultiplayerSupport), "FixRNGPos", null), null, null);
+        }
 
-		private static void FixRNGPre()
-		{
-			Rand.PushState(Find.TickManager.TicksAbs);
-		}
+        private static void FixRNGPre()
+        {
+            Rand.PushState(Find.TickManager.TicksAbs);
+        }
 
-		private static void FixRNGPos()
-		{
-			Rand.PopState();
-		}
+        private static void FixRNGPos()
+        {
+            Rand.PopState();
+        }
 
-		private static Harmony harmony = new Harmony("rimworld.regrowthcore.multiplayersupport");
-	}
+        private static readonly Harmony harmony = new Harmony("rimworld.regrowthcore.multiplayersupport");
+    }
 }
 
